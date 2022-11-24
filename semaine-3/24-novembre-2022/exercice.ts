d0e01a9691e31fa0b31b3685d6c21845fd27ceb9
Robin Hotton <Robin.hotton@hotmail.com>
2022-11-24 15:13:38 +0100

    docs: ajout de docs un peu partout

--- semaine-3/24-novembre-2022/exercice.ts
+++ semaine-3/24-novembre-2022/exercice.ts
@@ -3,8 +3,8 @@
 /**
  *  Fonction qui dit bonjour à 'prenom'
  */
-function bonjour(prenom:string) :string {
-    return `Bonjour ${prenom} !`;
+function bonjour(prenom: string): string {
+  return `Bonjour ${prenom} !`;
 }
 
 console.log(bonjour("Michel"));
@@ -13,159 +13,167 @@ console.log(bonjour("Michel"));
 
 /**
  * Fonction qui addition entre 2 nombres
- * @param nombre1 
- * @param nombre2 
+ * @param nombre1
+ * @param nombre2
  * @returns nombre1 + nombre2
  */
-function calcul(nombre1:number,nombre2:number) :number {
-    return nombre1+nombre2;
+function calcul(nombre1: number, nombre2: number): number {
+  return nombre1 + nombre2;
 }
-console.log(calcul(3,5));
+console.log(calcul(3, 5));
 
 // Exerice 3
 
 /**
  * Fonction qui annonce le gain ou la perte en €
- * @param coutFabrication 
- * @param prixVente 
+ * @param coutFabrication
+ * @param prixVente
  * @returns difference entre coutFabrication et prixVente
  */
-function gain(coutFabrication:number,prixVente:number) :string {
-    if (coutFabrication >= prixVente) {
-        return `Perte de ${coutFabrication - prixVente}€`
-    } else {
-        return `Gain de ${prixVente - coutFabrication}€`
-    }
+function gain(coutFabrication: number, prixVente: number): string {
+  if (coutFabrication >= prixVente) {
+    return `Perte de ${coutFabrication - prixVente}€`;
+  } else {
+    return `Gain de ${prixVente - coutFabrication}€`;
+  }
 }
-console.log(gain(15,15));
+console.log(gain(15, 15));
 
 // Exerice 4
 
 /**
  * Fonction qui sert a savoir le plus grand entre 3 nombres
- * @param nombre1 
- * @param nombre2 
- * @param nombre3 
+ * @param nombre1
+ * @param nombre2
+ * @param nombre3
  * @returns Le plus grand
  */
-function plusGrand(nombre1:number, nombre2:number, nombre3:number) :number {
-    if (nombre1 >= nombre2 && nombre1 >= nombre3) {
-        return nombre1;
-    } else if (nombre2 >= nombre3) {
-        return nombre2
-    } else {
-        return nombre3;
-    }
+function plusGrand(nombre1: number, nombre2: number, nombre3: number): number {
+  if (nombre1 >= nombre2 && nombre1 >= nombre3) {
+    return nombre1;
+  } else if (nombre2 >= nombre3) {
+    return nombre2;
+  } else {
+    return nombre3;
+  }
 }
-console.log(plusGrand(2,4,3));
+console.log(plusGrand(2, 4, 3));
 
 // Exerice 5
 
 /**
  * la fonction recoit une liste de nombre
- * @param nombres 
+ * @param nombres
  * @returns le plus grand
  */
-function plusGrandTab(...nombres:number[]) :number {
-    let plusGrand:number = nombres[0];
-    for (let index = 1; index < nombres.length; index++) {
-        if (nombres[index] > plusGrand) {
-            plusGrand = nombres[index];
-        }
+function plusGrandTab(...nombres: number[]): number {
+  let plusGrand: number = nombres[0];
+  for (let index = 1; index < nombres.length; index++) {
+    if (nombres[index] > plusGrand) {
+      plusGrand = nombres[index];
     }
-    return plusGrand;
+  }
+  return plusGrand;
 }
-console.log(plusGrandTab(12,5,3,14,14));
+console.log(plusGrandTab(12, 5, 3, 14, 14));
 
 // Exerice 6
 
 /**
- * 
+ *
  * @param prenom prenom de l'élève
  * @param notes utilise la fonction 'moyenne(notes:number[])' pour avoir la moyenne le l'élève
  * @returns prenom + moyenne + appreciation
  */
-function lazy(prenom:string, ...notes:number[]) :string {
-    const moy:number = moyenne(notes);
-    return `${prenom} : ${moy}, ${appreciation(moy)}`;
+function lazy(prenom: string, ...notes: number[]): string {
+  const moy: number = moyenne(notes);
+  return `${prenom} : ${moy}, ${appreciation(moy)}`;
 }
 
 /**
  * Fait la moyenne de toutes les notes
- * @param notes 
- * @returns moyenne
+ * @param notes
+ * @returns moyenne des notes
  */
-function moyenne(notes:number[]) :number {
-    let moyenne:number = notes [0];
-    for (let index = 1; index < notes.length; index++) {
-        moyenne += notes[index];
-    }
-    return moyenne/notes.length;
+function moyenne(notes: number[]): number {
+  let moyenne: number = notes[0];
+  for (let index = 1; index < notes.length; index++) {
+    moyenne += notes[index];
+  }
+  return moyenne / notes.length;
 }
 
 /**
  * Donne l'appréciation par rapport a la moyenne de l'élève
- * @param moyenne 
- * @returns appreciation
+ * @param moyenne
+ * @returns appreciation de l'élève
  */
-function appreciation(moyenne:number) :string {
-    let result:string = "";
-     if (moyenne > 20 || moyenne < 0) {
-        result = "Note non valide";
-    } else if (moyenne >= 19) {
-        result = "Très bien"
-    } else if (moyenne >= 15) {
-        result = "Bien"
-    } else if (moyenne >= 11) {
-        result = "Passable"
-    } else if (moyenne >= 5) {
-        result = "Insuffisant"
-    } else {
-        result = "Catastrophique"
-    }
-    return result;
+function appreciation(moyenne: number): string {
+  let result: string = "";
+  if (moyenne > 20 || moyenne < 0) {
+    result = "Note non valide";
+  } else if (moyenne >= 19) {
+    result = "Très bien";
+  } else if (moyenne >= 15) {
+    result = "Bien";
+  } else if (moyenne >= 11) {
+    result = "Passable";
+  } else if (moyenne >= 5) {
+    result = "Insuffisant";
+  } else {
+    result = "Catastrophique";
+  }
+  return result;
 }
 
-console.log(lazy("Corine de la Compta",12,15,10));
+console.log(lazy("Corine de la Compta", 12, 15, 10));
 
 // Exerice 7
 
 /**
- * permet le calcul entre 2 nombre avec uniquement les operateurs : '+' '-' '*' '/'
- * @param nombre1 
- * @param nombre2 
- * @param operateur 
- * @returns calcul nombre 1 et nombre 2 selon operator 
+ * permet le calcul entre 2 nombres
+ * @param nombre1
+ * @param nombre2
+ * @param operateur uniquement '+' '-' '*' '/'
+ * @returns calcul nombre 1 et nombre 2 selon operator
  */
-function operation(nombre1:number, nombre2:number, operateur:string) :number {
-    let result:number = 0;
-    if (operateur === "+") {
-        result = nombre1 + nombre2;
-    } else if (operateur === "-") {
-        result = nombre1 - nombre2;
-    } else if ( operateur === "*" ) {
-        result = nombre1 * nombre2;
-    } else if (operateur === "/") {
-        result = nombre1 / nombre2;
-    } else {
-        result = 0;
-    }
-    return result;
+function operation(
+  nombre1: number,
+  nombre2: number,
+  operateur: string
+): number {
+  let result: number = 0;
+  if (operateur === "+") {
+    result = nombre1 + nombre2;
+  } else if (operateur === "-") {
+    result = nombre1 - nombre2;
+  } else if (operateur === "*") {
+    result = nombre1 * nombre2;
+  } else if (operateur === "/") {
+    result = nombre1 / nombre2;
+  } else {
+    result = 0;
+  }
+  return result;
 }
-console.log(operation(12,2,'/'));
- 
+console.log(operation(12, 2, "/"));
 
 // Exerice 8
 
-function pyramide(taille:number):string {
-    let resultat:string = "";
-    for (let indexLigne = 0; indexLigne < taille; indexLigne++) {
-        for (let indexColone = 0; indexColone <= indexLigne; indexColone++) {
-            resultat += "*";
-        }
-        resultat += "\n";
+/**
+ * fabrique une pyramide horizontal de '*'
+ * @param taille taille de la pyramide
+ * @returns pyramide en *
+ */
+function pyramide(taille: number): string {
+  let resultat: string = "";
+  for (let indexLigne = 0; indexLigne < taille; indexLigne++) {
+    for (let indexColone = 0; indexColone <= indexLigne; indexColone++) {
+      resultat += "*";
     }
-    return resultat;
+    resultat += "\n";
+  }
+  return resultat;
 }
 console.log(pyramide(5));
 
@@ -174,30 +182,34 @@ console.log(pyramide(5));
 /**
  * liste de toutes les monnaies qui existe
  */
-const monnaie:number[] = [500,200,100,50,20,10,5,2,1,0.50,0.20,0.10,0.05,0.02,0.01];
+const monnaie: number[] = [
+  500, 200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01,
+];
 
 /**
  * permet de savoir se qu'il faut précisément rendre comme monnaie a la personne
- * @param somme 
+ * @param somme
  * @returns string avec tous les billets/pieces present
  */
-function billets(somme:number) :string {
-    let result:string = `Somme totale : ${somme}\n`;
-    let sommeCourante:number = somme;
+function billets(somme: number): string {
+  let result: string = `Somme totale : ${somme}\n`;
+  let sommeCourante: number = somme;
 
-    //permet de verifier si on une piece/un billet chaque monnaie qui existe
-    monnaie.forEach(monnaie => {
-        let nombreMonnaie:number = Math.floor(sommeCourante/monnaie);
-        sommeCourante = sommeCourante%monnaie;
-        if (monnaie >= 5 && nombreMonnaie > 0) { // on a des billets
-            result += `Billet de ${monnaie} : ${nombreMonnaie}\n`
-        } else if(monnaie < 5 && nombreMonnaie > 0) { // on a des pieces
-            result += `Pièce de ${monnaie} : ${nombreMonnaie}\n`
-        }
-    });
-    return result
+  //permet de verifier si on une piece/un billet chaque monnaie qui existe
+  monnaie.forEach((monnaie) => {
+    let nombreMonnaie: number = Math.floor(sommeCourante / monnaie);
+    sommeCourante = sommeCourante % monnaie;
+    // on a des billets
+    if (monnaie >= 5 && nombreMonnaie > 0) {
+      result += `Billet de ${monnaie} : ${nombreMonnaie}\n`;
+    }
+    // on a des pieces
+    else if (monnaie < 5 && nombreMonnaie > 0) {
+      result += `Pièce de ${monnaie} : ${nombreMonnaie}\n`;
+    }
+  });
+  return result;
 }
 console.log(billets(123.45));
 
 // Exercice 10
-
