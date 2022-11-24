f6947835031eedeb7d0d5bc5dd9cb7474dd990f0
Robin Hotton <Robin.hotton@hotmail.com>
2022-11-24 11:15:00 +0100

    exo 1

--- semaine-3/24-novembre-2022/exercice.ts
+++ semaine-3/24-novembre-2022/exercice.ts
@@ -1,8 +1,21 @@
 // Exerice 1
+function bonjour(prenom:string) :void {
+    console.log(`Bonjour ${prenom} !`);
+}
+
+bonjour("Michel")
 // Exerice 2
+
+
 // Exerice 3
+
+
 // Exerice 4
+
 // Exerice 5
+
 // Exerice 6
+
 // Exerice 7
+
 // Exerice 8
