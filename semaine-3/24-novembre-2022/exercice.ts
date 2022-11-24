d2a167bb87ba1fe74155ca683b6134e6c5435650
Robin Hotton <Robin.hotton@hotmail.com>
2022-11-24 11:20:09 +0100

    exo 2

--- semaine-3/24-novembre-2022/exercice.ts
+++ semaine-3/24-novembre-2022/exercice.ts
@@ -5,7 +5,10 @@ function bonjour(prenom:string) :void {
 
 bonjour("Michel")
 // Exerice 2
-
+function calcul(nombre1:number,nombre2:number) :Number {
+    return nombre1+nombre2;
+}
+console.log(calcul(3,5));
 
 // Exerice 3
 
