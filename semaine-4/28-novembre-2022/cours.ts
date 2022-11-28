fd788089b4e1b2ffc2c8cbd7e1f3b8bb02850391
Robin Hotton <Robin.hotton@hotmail.com>
2022-11-28 11:47:57 +0100

    refactor: changement de dossier

--- semaine-4/28-novembre-2022/cours.ts
+++ semaine-4/28-novembre-2022/cours.ts
@@ -1,7 +1,28 @@
-try {
-  // first
-} catch (error) {
-  // catch an error
-} finally {
-  // after a succesfull try
+// try {
+//   // first
+// } catch (error) {
+//   // catch an error
+// } finally {
+//   // after a succesfull try
+// }
+
+type Chat = {
+  nom: string;
+  race: string;
+  age: number;
+  poid: number;
+};
+
+const chat: Chat = {
+  nom: "pikachu",
+  race: "europeen",
+  age: 3,
+  poid: 4,
+};
+
+function getChat(...surnoms: string[]): string {
+  return `nom:${chat.nom}, race:${chat.race}, age:${chat.age}ans, poid:${
+    chat.poid
+  } kilos\nSes surnoms sont: ${surnoms.join(", ")}`;
 }
+console.log(getChat("chacha", "chachou", "pika", "gros tas"));
