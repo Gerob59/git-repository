7f5d31e579e9805f1b34ef840e6a2b01cd31ce4a
Robin Hotton <Robin.hotton@hotmail.com>
2022-12-03 12:24:36 +0100

    build: ajout d'un dossier effet :construction:
    j'ai rajouté les dossiers alteration/statut dans le dossier effet

--- autre/pokemon/Classes/effet/EffetAbstrait.ts
+++ autre/pokemon/Classes/effet/EffetAbstrait.ts
@@ -1,5 +1,5 @@
-import Effet from "../Interfaces/Effet";
-import Pokemon from "./Pokemon";
+import Effet from "../../Interfaces/Effet";
+import Pokemon from "../Pokemon";
 
 export default abstract class AbstractEffet implements Effet {
   nomAlteration: string;
