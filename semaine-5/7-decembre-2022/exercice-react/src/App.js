b214de960ff25e99386ca728b6c7a2d38ea730e8
Robin Hotton <Robin.hotton@hotmail.com>
2022-12-07 15:22:03 +0100

    feat: ajout d'un lien sur la homepage :sparkles:

--- semaine-5/7-decembre-2022/exercice-react/src/App.js
+++ semaine-5/7-decembre-2022/exercice-react/src/App.js
@@ -1,9 +1,13 @@
 import { Route, Routes } from "react-router";
+import { Link } from "react-router-dom";
 import Home from "./page/Home";
 
 export default function App() {
   return (
     <>
+      <ul>
+        <Link to="/">Acceuil</Link>
+      </ul>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
