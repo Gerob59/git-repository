bbf95b58bba06a8e2d4b8e1faa333c6ae8417c85
Robin Hotton <Robin.hotton@hotmail.com>
2022-12-07 14:19:35 +0100

    ajout de : BrowserRouter / Routes / Route / Link :sparkles:

--- semaine-5/6-decembre-2022/projetreact/src/App.js
+++ semaine-5/6-decembre-2022/projetreact/src/App.js
@@ -1,33 +1,31 @@
 import "./app.css";
-import Contact from "./Components/contact/Contact";
+
+// import Bouton from "./Components/bouton/Bouton";
+// import CheckBox from "./Components/checkbox/Checkbox";
+// import Checkboxbutton from "./Components/Checkboxbutton";
+// import { useState } from "react";
+import { Route, Routes } from "react-router";
+import ContactList from "./Components/contact/ContactList";
 import Count from "./Components/count/Count";
-import personnes from "./Data";
-import Bouton from "./Components/bouton/Bouton";
-import CheckBox from "./Components/checkbox/Checkbox";
-import Checkboxbutton from "./Components/Checkboxbutton";
-import { useState } from "react";
 
 export default function App() {
-  let [checked, setChecked] = useState(false);
-  const click = () => setChecked(!checked);
+  // let [checked, setChecked] = useState(false);
+  // const click = () => setChecked(!checked);
 
   return (
     <>
+      <Routes>
+        <Route path="/contact" element={<ContactList />} />
+        <Route path="/count" element={<Count />} />
+      </Routes>
+      {/*     
       <Count />
-      <div className="Contacts">
-        {personnes.map((personne, index) => (
-          <Contact key={index} {...personne} />
-        ))}
-      </div>
       <CheckBox etat={checked} onChange={click} />
       <Bouton etat={checked} onChange={click} />
-
       <CheckBox etat={checked} onChange={click} />
       <Bouton etat={checked} onChange={click} />
-
       <Checkboxbutton />
-      <Checkboxbutton />
-      <Checkboxbutton />
+      <Checkboxbutton /> */}
     </>
   );
 }
