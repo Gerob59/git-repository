e42937b5920f81261a1b37fd72ae147035ca4bca
Robin Hotton <Robin.hotton@hotmail.com>
2022-12-12 14:45:10 +0100

    feat(canvas): images ready :sparkles:

--- autre/pokemon/Classes/canvas/Canvas.js
+++ autre/pokemon/Classes/canvas/Canvas.js
@@ -1,14 +1,16 @@
 const myCanvas = document.querySelector("canvas");
 let ctx = myCanvas.getContext("2d");
-ctx.fillStyle = "green";
-ctx.fillRect(0, 0, myCanvas.width, myCanvas.height);
-let pkm;
-make_base();
+let droite = 0;
+let bas = 0;
 
-function make_base() {
-  pkm = new Image();
-  pkm.src = "../../Images/pikachu2.png";
-  pkm.onload = function () {
-    context.drawImage(pkm, 0, 0, 100, 100);
+const drawImage = (url) => {
+  const pkm = new Image();
+  pkm.src = url;
+  pkm.onload = () => {
+    ctx.drawImage(pkm, droite, bas, 100, 100);
+    droite += 100;
   };
-}
+};
+
+drawImage("../../Images/pikachu.jfif");
+drawImage("../../Images/pikachu2.png");
