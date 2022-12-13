0ae39fe79f7676faab9fd37e60cc970a8e9529d0
Robin Hotton <Robin.hotton@hotmail.com>
2022-12-13 09:07:36 +0100

    boutons

--- autre/pokemon/Classes/canvas/Canvas.js
+++ autre/pokemon/Classes/canvas/Canvas.js
@@ -1,14 +1,15 @@
 const myCanvas = document.querySelector("canvas");
 let ctx = myCanvas.getContext("2d");
-let droite = 0;
-let bas = 0;
+let axeX = 0;
+let axeY = 0;
+let taille = 75;
 
 const drawImage = (url) => {
   const pkm = new Image();
   pkm.src = url;
   pkm.onload = () => {
-    ctx.drawImage(pkm, droite, bas, 75, 75);
-    droite += 75;
+    ctx.drawImage(pkm, axeX, axeY, taille, taille);
+    axeX += taille;
   };
 };
 
