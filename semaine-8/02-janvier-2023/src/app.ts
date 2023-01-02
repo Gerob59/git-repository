8ed2c6f35356461a9b7328a23ccfda741ec9dc92
Robin Hotton <Robin.hotton@hotmail.com>
2023-01-02 12:55:37 +0100

    feat: todoModel :sparkles:

--- semaine-8/02-janvier-2023/src/app.ts
+++ semaine-8/02-janvier-2023/src/app.ts
@@ -1,11 +1,28 @@
 import Express, { Request, Response } from "express";
+import TodoModel from "./model/todo.model";
 
+// instance de Express
 const app = Express();
+// instance du routeur de Express
+const router = Express.Router();
 
-app.use("/", (req: Request, res: Response) => {
+// route via router
+router.get("/test", (req: Request, res: Response) => {
+  res.send("page de test");
+});
+// appel fu router
+app.use(router);
+
+// route par defaut
+app.get("/", (req: Request, res: Response) => {
+  const model: TodoModel = new TodoModel("task 1");
+  console.log(
+    `id : ${model.id}, task : ${model.task}, completed : ${model.completed}`
+  );
   res.send("c'est tout bon");
 });
 
+// définition du port d'écoute
 app.listen("3000", () => {
   console.log(`server lancé sur le port 3000`);
 });
---
+++ semaine-8/02-janvier-2023/src/model/todo.model.ts
@@ -0,0 +1,32 @@
+export default class TodoModel {
+  private _id: number;
+  private _task: string;
+  private _completed: boolean;
+  private static count: number = 0;
+
+  constructor(task: string) {
+    this._id = ++TodoModel.count;
+    this._task = task;
+    this._completed = false;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get task() {
+    return this._task;
+  }
+
+  get completed() {
+    return this._completed;
+  }
+
+  set task(task: string) {
+    this._task = task;
+  }
+
+  public finished() {
+    this._completed = !this._completed;
+  }
+}
