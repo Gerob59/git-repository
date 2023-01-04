9ac76e2dad1e638641c8824715ac00fbfcd6bf4c
Robin Hotton <Robin.hotton@hotmail.com>
2023-01-04 15:21:32 +0100

    docs(todo.controller): methodes http :memo:

--- semaine-8/02-janvier-2023/src/controller/todo.controller.ts
+++ semaine-8/02-janvier-2023/src/controller/todo.controller.ts
@@ -17,26 +17,51 @@ export default class TodoController {
     this.service = service;
   }
 
+  /**
+   * Methode GET de HTTP pour recupérer toutes les données de la base de donnée.
+   * @param req La requete HTTP avec des informations pour le serveur.
+   * @param res La reponse du serveur pour la requete HTTP.
+   */
   public getAll = (req: Request, res: Response): void => {
     res.send(this.service.getAll());
   };
 
+  /**
+   * Methode GET de HTTP pour recupérer une donnée de la base de donnée grâce à son id.
+   * @param req La requete HTTP avec des informations pour le serveur.
+   * @param res La reponse du serveur pour la requete HTTP.
+   */
   public getById = (req: Request, res: Response): void => {
     const id: number = Number(req.params.id);
     res.send(this.service.getById(id));
   };
 
+  /**
+   * Methode DELETE de HTTP pour supprimer une donnée de la base de donnée grâce à son id.
+   * @param req La requete HTTP avec des informations pour le serveur.
+   * @param res La reponse du serveur pour la requete HTTP.
+   */
   public deleteById = (req: Request, res: Response): void => {
     const id: number = Number(req.params.id);
     res.send(this.service.deleteById(id));
   };
 
+  /**
+   * Methode POST de HTTP pour créer une donnée dans la base de donnée grâce à un JSON dans son body.
+   * @param req La requete HTTP avec des informations pour le serveur.
+   * @param res La reponse du serveur pour la requete HTTP.
+   */
   public create = (req: Request, res: Response): void => {
     const task: string = req.body.task;
     const todo: TodoModel = this.service.create(task);
     res.send(todo);
   };
 
+  /**
+   * Methode PUT de HTTP pour modifier les données d'un objet dans la base de donnée grâce à un JSON dans son body, ou la créer si elle n'existe pas.
+   * @param req La requete HTTP avec des informations pour le serveur.
+   * @param res La reponse du serveur pour la requete HTTP.
+   */
   public createOrUpdate = (req: Request, res: Response): void => {
     const id: number = +req.params.id;
     const todo: TodoModel = req.body;
@@ -44,6 +69,11 @@ export default class TodoController {
     res.send(data);
   };
 
+  /**
+   * Methode PATCH de HTTP pour modifier les données d'un objet dans la base de donnée grâce à un JSON dans son body.
+   * @param req La requete HTTP avec des informations pour le serveur.
+   * @param res La reponse du serveur pour la requete HTTP.
+   */
   public update = (req: Request, res: Response): void => {
     const id: number = +req.params.id;
     const todo: TodoModel = req.body;
